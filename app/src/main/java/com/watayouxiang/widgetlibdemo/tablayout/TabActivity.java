b45a44d2839e66b23c495a7f4a036c4f6f736591
watayouxiang <watayouxiang@qq.com>
2019-09-27 15:56:27 +0800

    .

--- app/src/main/java/com/watayouxiang/widgetlibdemo/tablayout/TabActivity.java
+++ app/src/main/java/com/watayouxiang/widgetlibdemo/tablayout/TabActivity.java
@@ -13,6 +13,7 @@ import com.watayouxiang.widgetlibdemo.R;
 import com.watayouxiang.widgetlibrary.tablayout.TaoOnItemClickListener;
 import com.watayouxiang.widgetlibrary.tablayout.TaoViewHolder;
 import com.watayouxiang.widgetlibrary.tablayout.custom.CategoryTabAdapter;
+import com.watayouxiang.widgetlibrary.tablayout.custom.FilterTabAdapter;
 import com.watayouxiang.widgetlibrary.tablayout.custom.NavigateTabAdapter;
 
 import java.util.ArrayList;
@@ -20,6 +21,11 @@ import java.util.List;
 
 public class TabActivity extends DemoActivity {
     private NavigateTabAdapter mTabAdapter;
+    private RecyclerView tabLayout;
+    private RecyclerView tabLayout2;
+    private RecyclerView tabLayout3;
+    private RecyclerView tabLayout4;
+    private ViewPager viewPager;
 
     @Override
     protected int getHolderViewId() {
@@ -53,35 +59,36 @@ public class TabActivity extends DemoActivity {
     @Override
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
+        tabLayout = findViewById(R.id.rv_tabLayout);
+        tabLayout2 = findViewById(R.id.rv_tabLayout2);
+        tabLayout3 = findViewById(R.id.rv_tabLayout3);
+        tabLayout4 = findViewById(R.id.rv_tabLayout4);
+        viewPager = findViewById(R.id.vp_common);
         initTabLayout();
         initTabLayout2();
         initTabLayout3();
+        initTabLayout4();
+    }
+
+    private void initTabLayout4() {
+        viewPager.setAdapter(new VpAdapter(getSupportFragmentManager(), getTestData()));
+        FilterTabAdapter tabAdapter = new FilterTabAdapter(tabLayout4);
+        tabAdapter.setViewPager(viewPager);
     }
 
     private void initTabLayout3() {
-        RecyclerView tabLayout = findViewById(R.id.rv_tabLayout3);
-        final CategoryTabAdapter tabAdapter = new CategoryTabAdapter(tabLayout);
-        tabAdapter.setOnItemClickListener(new TaoOnItemClickListener() {
-            @Override
-            public boolean onItemClick(TaoViewHolder viewHolder) {
-                String text = tabAdapter.getData().get(viewHolder.getLayoutPosition());
-                Toast.makeText(viewHolder.itemView.getContext(), text, Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-        tabAdapter.setNewData(getTestData());
+        viewPager.setAdapter(new VpAdapter(getSupportFragmentManager(), getTestData()));
+        CategoryTabAdapter tabAdapter = new CategoryTabAdapter(tabLayout3);
+        tabAdapter.setViewPager(viewPager);
     }
 
     private void initTabLayout2() {
-        RecyclerView tabLayout = findViewById(R.id.rv_tabLayout2);
-        ViewPager viewPager = findViewById(R.id.viewPager2);
         viewPager.setAdapter(new VpAdapter(getSupportFragmentManager(), getTestData()));
-        NavigateTabAdapter tabAdapter = new NavigateTabAdapter(tabLayout);
+        NavigateTabAdapter tabAdapter = new NavigateTabAdapter(tabLayout2);
         tabAdapter.setViewPager(viewPager);
     }
 
     private void initTabLayout() {
-        RecyclerView tabLayout = findViewById(R.id.rv_tabLayout);
         mTabAdapter = new NavigateTabAdapter(tabLayout);
         mTabAdapter.setOnItemClickListener(new TaoOnItemClickListener() {
             @Override
---
+++ widgetlibrary/src/main/java/com/watayouxiang/widgetlibrary/tablayout/custom/FilterTabAdapter.java
@@ -0,0 +1,32 @@
+package com.watayouxiang.widgetlibrary.tablayout.custom;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.watayouxiang.widgetlibrary.R;
+import com.watayouxiang.widgetlibrary.tablayout.TaoTabAdapter;
+import com.watayouxiang.widgetlibrary.tablayout.TaoViewHolder;
+
+public class FilterTabAdapter extends TaoTabAdapter {
+    public FilterTabAdapter(@NonNull RecyclerView recyclerView) {
+        super(recyclerView);
+    }
+
+    @Override
+    protected int getItemViewLayoutId() {
+        return R.layout.tab_filter;
+    }
+
+    @Override
+    protected void convert(@NonNull TaoViewHolder holder, String text, boolean select) {
+        TextView tv_txt = holder.itemView.findViewById(R.id.tv_txt);
+        ImageView iv_arrow = holder.itemView.findViewById(R.id.iv_arrow);
+        tv_txt.setText(String.valueOf(text));
+        tv_txt.setSelected(select);
+        iv_arrow.setVisibility(select ? View.VISIBLE : View.GONE);
+    }
+}
