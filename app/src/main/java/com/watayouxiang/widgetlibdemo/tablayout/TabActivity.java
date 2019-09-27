08304b920e3328c6f5095a8404a7ec4fa4c3803f
watayouxiang <watayouxiang@qq.com>
2019-09-27 18:18:26 +0800

    .

--- app/src/main/java/com/watayouxiang/widgetlibdemo/tablayout/TabActivity.java
+++ app/src/main/java/com/watayouxiang/widgetlibdemo/tablayout/TabActivity.java
@@ -20,10 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TabActivity extends DemoActivity {
-    private NavigateTabAdapter tabAdapter;
-    private NavigateTabAdapter tabAdapter2;
-    private CategoryTabAdapter tabAdapter3;
-    private FilterTabAdapter tabAdapter4;
+    private NavigateTabAdapter navigateTabAdapter;
+    private CategoryTabAdapter categoryTabAdapter;
+    private FilterTabAdapter filterTabAdapter;
 
     @Override
     protected int getHolderViewId() {
@@ -36,28 +35,25 @@ public class TabActivity extends DemoActivity {
                 .addClick("清空数据", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        tabAdapter.setNewData(null);
-                        tabAdapter2.setNewData(null);
-                        tabAdapter3.setNewData(null);
-                        tabAdapter4.setNewData(null);
+                        navigateTabAdapter.setNewData(null);
+                        categoryTabAdapter.setNewData(null);
+                        filterTabAdapter.setNewData(null);
                     }
                 })
                 .addClick("设置新数据", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        tabAdapter.setNewData(getTestData());
-                        tabAdapter2.setNewData(getTestData());
-                        tabAdapter3.setNewData(getTestData());
-                        tabAdapter4.setNewData(getTestData());
+                        navigateTabAdapter.setNewData(getNavigateData());
+                        categoryTabAdapter.setNewData(getCategoryData());
+                        filterTabAdapter.setNewData(getFilterData());
                     }
                 })
                 .addClick("选中最后一个", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        tabAdapter.setCurrentItem(tabAdapter.getItemCount() - 1);
-                        tabAdapter2.setCurrentItem(tabAdapter2.getItemCount() - 1);
-                        tabAdapter3.setCurrentItem(tabAdapter3.getItemCount() - 1);
-                        tabAdapter4.setCurrentItem(tabAdapter4.getItemCount() - 1);
+                        navigateTabAdapter.setCurrentItem(navigateTabAdapter.getItemCount() - 1);
+                        categoryTabAdapter.setCurrentItem(categoryTabAdapter.getItemCount() - 1);
+                        filterTabAdapter.setCurrentItem(filterTabAdapter.getItemCount() - 1);
                     }
                 })
                 ;
@@ -67,61 +63,82 @@ public class TabActivity extends DemoActivity {
     protected void initData(Bundle savedInstanceState) {
         super.initData(savedInstanceState);
         ViewPager viewPager = findViewById(R.id.vp_common);
-        RecyclerView tabLayout = findViewById(R.id.rv_tabLayout);
         RecyclerView tabLayout2 = findViewById(R.id.rv_tabLayout2);
         RecyclerView tabLayout3 = findViewById(R.id.rv_tabLayout3);
         RecyclerView tabLayout4 = findViewById(R.id.rv_tabLayout4);
-        initTabLayout(tabLayout);
-        initTabLayout2(tabLayout2, viewPager);
-        initTabLayout3(tabLayout3, viewPager);
-        initTabLayout4(tabLayout4, viewPager);
+        initNavigateTabAdapter(tabLayout2, viewPager);
+        initCategoryTabAdapter(tabLayout3);
+        initFilterTabAdapter(tabLayout4);
     }
 
-    private void initTabLayout4(RecyclerView recyclerView, ViewPager viewPager) {
-        viewPager.setAdapter(new VpAdapter(getSupportFragmentManager(), getTestData()));
-        tabAdapter4 = new FilterTabAdapter(recyclerView);
-        tabAdapter4.setViewPager(viewPager);
-    }
-
-    private void initTabLayout3(RecyclerView recyclerView, ViewPager viewPager) {
-        viewPager.setAdapter(new VpAdapter(getSupportFragmentManager(), getTestData()));
-        tabAdapter3 = new CategoryTabAdapter(recyclerView);
-        tabAdapter3.setViewPager(viewPager);
-    }
-
-    private void initTabLayout2(RecyclerView recyclerView, ViewPager viewPager) {
-        viewPager.setAdapter(new VpAdapter(getSupportFragmentManager(), getTestData()));
-        tabAdapter2 = new NavigateTabAdapter(recyclerView);
-        tabAdapter2.setViewPager(viewPager);
-    }
-
-    private void initTabLayout(RecyclerView recyclerView) {
-        tabAdapter = new NavigateTabAdapter(recyclerView);
-        tabAdapter.setOnItemClickListener(new TaoOnItemClickListener() {
+    private void initFilterTabAdapter(RecyclerView recyclerView) {
+        filterTabAdapter = new FilterTabAdapter(recyclerView);
+        filterTabAdapter.setNewData(getFilterData());
+        filterTabAdapter.setOnItemClickListener(new TaoOnItemClickListener() {
             @Override
             public boolean onItemClick(TaoViewHolder viewHolder) {
-                String text = tabAdapter.getData().get(viewHolder.getLayoutPosition());
-                Toast.makeText(viewHolder.itemView.getContext(), text, Toast.LENGTH_SHORT).show();
+                Toast.makeText(
+                        viewHolder.itemView.getContext(),
+                        filterTabAdapter.getData().get(viewHolder.getLayoutPosition()),
+                        Toast.LENGTH_SHORT
+                ).show();
                 return true;
             }
         });
-        tabAdapter.setNewData(getTestData());
     }
 
-    private List<String> getTestData() {
+    private void initCategoryTabAdapter(RecyclerView recyclerView) {
+        categoryTabAdapter = new CategoryTabAdapter(recyclerView);
+        categoryTabAdapter.setNewData(getCategoryData());
+        categoryTabAdapter.setOnItemClickListener(new TaoOnItemClickListener() {
+            @Override
+            public boolean onItemClick(TaoViewHolder viewHolder) {
+                Toast.makeText(
+                        viewHolder.itemView.getContext(),
+                        categoryTabAdapter.getData().get(viewHolder.getLayoutPosition()),
+                        Toast.LENGTH_SHORT
+                ).show();
+                return true;
+            }
+        });
+    }
+
+    private void initNavigateTabAdapter(RecyclerView recyclerView, ViewPager viewPager) {
+        viewPager.setAdapter(new VpAdapter(getSupportFragmentManager(), getNavigateData()));
+        navigateTabAdapter = new NavigateTabAdapter(recyclerView);
+        navigateTabAdapter.setViewPager(viewPager);
+    }
+
+    private List<String> getNavigateData() {
         List<String> list = new ArrayList<>();
         list.add("推荐");
         list.add("初级课程");
         list.add("中级课程");
         list.add("高级课程");
         list.add("VIP课程");
-        list.add("电视剧");
-        list.add("电影");
-        list.add("综艺");
-        list.add("动漫");
-        list.add("杭州");
-        list.add("小视频");
-        list.add("体育");
+        list.add("白卡课程");
+        list.add("紫卡课程");
+        list.add("橙卡课程");
+        list.add("金色史诗课程");
+        list.add("免费课程");
+        return list;
+    }
+
+    private List<String> getCategoryData() {
+        List<String> list = new ArrayList<>();
+        list.add("入门");
+        list.add("进阶");
+        list.add("实战");
+        list.add("中阶");
+        list.add("高阶");
+        return list;
+    }
+
+    private List<String> getFilterData() {
+        List<String> list = new ArrayList<>();
+        list.add("热门");
+        list.add("价格");
+        list.add("最新");
         return list;
     }
 }
