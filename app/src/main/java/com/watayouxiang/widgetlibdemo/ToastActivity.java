abbc751eab1963de1834dc918ac60d1dc5fff6a4
watayouxiang <watayouxiang@qq.com>
2019-10-29 17:27:59 +0800

    .

--- app/src/main/java/com/watayouxiang/widgetlibdemo/ToastActivity.java
+++ app/src/main/java/com/watayouxiang/widgetlibdemo/ToastActivity.java
@@ -1,5 +1,6 @@
 package com.watayouxiang.widgetlibdemo;
 
+import android.content.Context;
 import android.view.View;
 
 import com.dovar.dtoast.NBToast;
@@ -9,29 +10,30 @@ import com.watayouxiang.demoshell.ListData;
 public class ToastActivity extends ListActivity {
     @Override
     protected ListData getListData() {
+        final Context context = ToastActivity.this;
         return new ListData()
                 .addClick("black_showShort", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        NBToast.black_showShort(v.getContext().getApplicationContext(), "这是一个不受限制的吐司");
+                        NBToast.black_showShort(context, "这是一个不受限制的吐司");
                     }
                 })
                 .addClick("black_showLong", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        NBToast.black_showLong(v.getContext().getApplicationContext(), "这是一个不受限制的吐司");
+                        NBToast.black_showLong(context, "这是一个不受限制的吐司");
                     }
                 })
                 .addClick("white_showShort", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        NBToast.white_showShort(v.getContext().getApplicationContext(), "这是一个不受限制的吐司");
+                        NBToast.white_showShort(context, "这是一个不受限制的吐司");
                     }
                 })
                 .addClick("white_showLong", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        NBToast.white_showLong(v.getContext().getApplicationContext(), "这是一个不受限制的吐司");
+                        NBToast.white_showLong(context, "这是一个不受限制的吐司");
                     }
                 });
     }
@@ -39,6 +41,6 @@ public class ToastActivity extends ListActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        NBToast.cancelAll();
+        NBToast.cancelAllToast();
     }
 }
